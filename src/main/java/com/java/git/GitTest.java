a035e533b5fab2093485f830a07557978aba55ac
hhz <1536845160@qq.com>
2022-11-12 18:47:17 +0800

    hot-fix 3 commit

--- src/main/java/com/java/git/GitTest.java
+++ src/main/java/com/java/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("aaaaaa");
+        System.out.println("ASJIJ");
     }
 }
