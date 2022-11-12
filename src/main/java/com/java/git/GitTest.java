5c417ae24b54138b24212f5381eb3d7ea0e5cebf
hhz <1536845160@qq.com>
2022-11-12 18:26:36 +0800

    first commit

--- src/main/java/com/java/git/GitTest.java
+++ src/main/java/com/java/git/GitTest.java
@@ -3,5 +3,6 @@ package com.java.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
